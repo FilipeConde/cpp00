6dfa0d8a7184ceb3409ad587f678131bbd240edd
Filipe Conde <fconde-p@student.42sp.org.br>
2026-09-19 15:22:24 -0300

    feat: implementa escrita de texto para execução sem parâmetros.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int ac, char **av){
-    std::cout << "Teste de escrita\n";
+    // writes single string input;
+    // writes sequence of strings from input;
+    // writes message if no input;
+
+    if(ac = 1){
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+    }
     return (0);
 }
