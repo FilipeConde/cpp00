278415204cbd0c8c4f90bef0e1d5061dd96a4b65
Filipe Conde <fconde-p@student.42sp.org.br>
2026-09-19 15:35:21 -0300

    feat: escreve cadeia de inputs na saída padrão.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,11 +4,19 @@ int main(int ac, char **av){
     // writes single string input;
     // writes sequence of strings from input;
     // writes message if no input;
+    // should write in uppercase;
+    int count = 1;
 
-    std::cout << ac << "\n";
     if(ac == 1){
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
     }
-    
+    if(ac > 1){
+        while(count < ac){
+            std::cout << av[count];
+            count++;
+        }
+        std::cout << "\n";
+    }
+
     return (0);
 }
